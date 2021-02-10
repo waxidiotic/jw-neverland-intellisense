2621408fa2dffaa305a3bfc70b1c45f606b95894
Alex Bussey <abussey@jwplayer.com>
2021-02-09 19:24:40 -0500

    Read data util

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { getColorsFromRepo } from "./data/scripts/getColors";
 import { saveDataToJSON } from "./util";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "jw-neverland-intellisense" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
+  vscode.commands.registerCommand(
     "jw-neverland-intellisense.helloWorld",
     async () => {
       const colors = await getColorsFromRepo();
       await saveDataToJSON(context, "colors", colors);
     }
   );
-
-  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate(): void {
+  // no-op
   return;
 }
--- src/util.ts
+++ src/util.ts
@@ -16,7 +16,24 @@ export const saveDataToJSON = async (
     await vscode.workspace.fs.writeFile(writePath, writeData);
     vscode.window.showInformationMessage(`Neverland: Saved ${type} data.`);
   } catch (error) {
-    vscode.window.showErrorMessage(`Neverland: Could not write to JSON.`);
+    vscode.window.showErrorMessage(`Neverland: Could not write data.`);
+    console.error(error);
+  }
+};
+
+export const readDataFromJSON = async (
+  context: vscode.ExtensionContext,
+  type: string
+): Promise<ColorsCollection | void> => {
+  const readPath = vscode.Uri.joinPath(
+    context.globalStorageUri,
+    `${type}.json`
+  );
+  try {
+    const data = JSON.stringify(await vscode.workspace.fs.readFile(readPath));
+    return JSON.parse(data);
+  } catch (error) {
+    vscode.window.showErrorMessage(`Neverland: Could not read data.`);
     console.error(error);
   }
 };
